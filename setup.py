a34b77cb3bed753815f9be7e4787160f9d812d7d
longshengwang <wanglongshengdf@126.com>
2020-11-25 16:45:29 +0800

    modify version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
       name="m_check",
-      version="1.0.0",
+      version="1.0.1",
       keywords='mars check',
       description="mars check command",
       author="Nocsys",
